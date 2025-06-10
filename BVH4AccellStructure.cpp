7cf0d5c02e966e5e3057d037c7b3664825c0646d
Daniel Banski <daniel.banski.1997@gmail.com>
2025-06-10 18:08:05 +0100

    General structure for the BVH4

--- BVH4AccellStructure.cpp
+++ BVH4AccellStructure.cpp
@@ -1,52 +1,69 @@
 #include "BVH4AccellStructure.h"
 
-//#include "BVHAccellStructure.h"
-//#include "AABB.h"
-//
-//// --------------------------------------------------------------------------------
-//BVH4AccellStructure::BVH4AccellStructure(const BVHAccellStructure* bvhAccellStructure)
-//{
-//	const ConstructResult cr = ConstructBVH4Node(bvhAccellStructure, 0u);
-//}
-//
-//// --------------------------------------------------------------------------------
-//ConstructResult BVH4AccellStructure::ConstructBVH4Node(const BVHAccellStructure* bvhAccellStructure, const uint32_t grandParentIndex)
-//{	
-//	// If it's a triangle, the index is negative
-//	// If the triangle 
-//	//Get the nodes grand children
-//	const BVHInnerNode grandparent = bvhAccellStructure->GetInnerNode(grandParentIndex);
-//
-//	const BVHInnerNode leftParent = bvhAccellStructure->GetInnerNode(grandparent.m_leftChild);
-//	const BVHInnerNode rightParent = bvhAccellStructure->GetInnerNode(grandparent.m_rightChild);
-//
-//	ConstructResult cr;
-//
-//	BVH4InnerNode bvh4Node;
-//	const uint32_t index = (uint32_t)m_bvh4InnerNodes.size();
-//	m_bvh4InnerNodes.push_back(bvh4Node);
-//
-//	const ConstructResult child0 = ConstructBVH4Node(bvhAccellStructure, leftParent.m_leftChild);
-//	const ConstructResult child1 = ConstructBVH4Node(bvhAccellStructure, leftParent.m_rightChild);
-//	const ConstructResult child2 = ConstructBVH4Node(bvhAccellStructure, rightParent.m_leftChild);
-//	const ConstructResult child3 = ConstructBVH4Node(bvhAccellStructure, rightParent.m_leftChild);
-//
-//	bvh4Node.m_child[0u] = child0.m_index;
-//	bvh4Node.m_child[1u] = child1.m_index;
-//	bvh4Node.m_child[2u] = child2.m_index;
-//	bvh4Node.m_child[3u] = child3.m_index;
-//
-//	bvh4Node.m_bbox[0u] = leftParent.m_leftAABB;
-//	bvh4Node.m_bbox[1u] = leftParent.m_rightAABB;
-//	bvh4Node.m_bbox[2u] = rightParent.m_leftAABB;
-//	bvh4Node.m_bbox[3u] = rightParent.m_rightAABB;
-//
-//	cr.m_index = index;
-//	// the aabb is irrelevant
-//
-//	return cr;
-//}
+#include "BVH2AccellStructure.h"
+#include "AABB.h"
 
+// --------------------------------------------------------------------------------
+BVH4AccellStructure::BVH4AccellStructure(const BVH2AccellStructure* bvh2AccellStructure)
+{
+	assert(bvh2AccellStructure != nullptr);
+	// Add getter for the size of the internal node vector, and check it is not empty
+	const uint32_t bvhRootIndex = BuildBVH4NodeFromBVH2Node(bvh2AccellStructure, 0);
+	assert(bvh == 0u);
+	(void)bvhRootIndex;
+}
 
+void RecursiveGetChildren(const BVH2AccellStructure* bvh2AccellStructure, const uint32_t& root, const AABB& box, const uint32_t depth, uint32_t* children, AABB* boxes,  uint32_t& addedChildren)
+{
+	assert(bvh2AccellStructure != nullptr);
+	assert(depth < 2u);
+	assert(children != nullptr);
+	assert(addedChildren < 4u);
 
+	const bool isTriangle = root >> 31u;
 
+	if (isTriangle || depth > 1u)
+	{
+		*children = root;
+		*boxes = box;
+		addedChildren++;
+	}
+	else
+	{
+		const BVH2InnerNode rootNode = bvh2AccellStructure->GetInnerNode(root);
+
+		RecursiveGetChildren(bvh2AccellStructure, rootNode.m_leftChild, rootNode.m_leftAABB, depth + 1, children + addedChildren, boxes + addedChildren, addedChildren);
+		RecursiveGetChildren(bvh2AccellStructure, rootNode.m_rightChild, rootNode.m_rightAABB, depth + 1, children + addedChildren, boxes + addedChildren, addedChildren);
+	}
+}
+
+// --------------------------------------------------------------------------------
+uint32_t BVH4AccellStructure::BuildBVH4NodeFromBVH2Node(const BVH2AccellStructure* bvh2AccellStructure, const uint32_t subtreeRootIndex)
+{
+	// Get children in local subtree
+	uint32_t children[4u];
+	AABB boxes[4u];
+	uint32_t addedChildren = 0u;
+	RecursiveGetChildren(bvh2AccellStructure, subtreeRootIndex, AABB(), 0, children, boxes, addedChildren);
+	assert(addedChildren >= 1u);
+	assert(addedChildren <= 4u);
+
+	BVH4InnerNode node;
+	const uint32_t bvh4InnerNodeIndex = (uint32_t)m_bvh4InnerNodes.size();
+	m_bvh4InnerNodes.push_back(node);
+
+	for (uint32_t childIndex = 0; childIndex < addedChildren; childIndex++)
+	{
+		node.m_child[childIndex] = BuildBVH4NodeFromBVH2Node(bvh2AccellStructure, children[childIndex]);
+		node.m_bbox[childIndex] = boxes[childIndex];
+	}
+
+	// Create dummies
+	for (uint32_t childIndex = addedChildren; childIndex < 4u; childIndex++)
+	{
+		node.m_child[childIndex] = 0x7ffffff;
+		node.m_bbox[childIndex] = AABB();
+	}
+
+	return bvh4InnerNodeIndex;
+}
--- BVH4AccellStructure.h
+++ BVH4AccellStructure.h
@@ -4,6 +4,12 @@
 
 class BVH2AccellStructure;
 
+struct BVH4ConstructResult
+{
+	uint32_t m_bvh4Index;
+	AABB m_aabb;
+};
+
 struct BVH4InnerNode
 {
 	AABB m_bbox[4u];
@@ -14,9 +20,8 @@ class BVH4AccellStructure
 {
 public:
 
-	BVH4AccellStructure(const BVH2AccellStructure* bvhAccellStructure);
-
-	ConstructResult ConstructBVH4Node(const BVH2AccellStructure* bvhAccellStructure, const uint32_t grandParentIndex);
+	BVH4AccellStructure(const BVH2AccellStructure* bvh2AccellStructure);
+	uint32_t BuildBVH4NodeFromBVH2Node(const BVH2AccellStructure * bvhAccellStructure, const uint32_t start);
 
 
 private:

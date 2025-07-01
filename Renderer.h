53796b0d4445a28228f3bde31806aa1193327391
Daniel Banski <daniel.banski.1997@gmail.com>
2025-07-01 01:13:13 +0100

    Remove sphere, plane and quad support

--- Renderer.h
+++ Renderer.h
@@ -30,11 +30,6 @@ public:
 private:
 
 	void RegenerateViewSpaceDirections(Framebuffer* framebuffer);
-	template<bool T_acceptAnyHit>
-	void HitSphere(const Ray& ray, const float tMin, float& tMax, HitResult& out_hitResult);
-	void HitPlane(const Ray& ray, const float tMin, float& tMax, const float distance, const Vector3& normalizedPlaneNormal, Vector3 colour, HitResult& out_hitResult);
-	void HitQuad(const Ray& ray, const float tMin, float& tMax, HitResult& out_hitResult);
-	bool IsInsideQuad(const float alpha, const float beta);
 
 	// Debug build contains an extra ray index paramater
 	template<bool T_acceptAnyHit>
@@ -45,13 +40,6 @@ private:
 	template<bool T_acceptAnyHit>
 	HitResult TraceRay(Ray& ray, const uint32_t rayIndex, const float tMin);
 
-	std::vector<Vector3> m_sphereList;
-	std::vector<Vector3> m_sphereColours;
-
-	std::vector<Vector3> m_quadList;
-	std::vector<Vector3> m_quadColours;
-	std::vector<Vector3> m_quadUs;
-	std::vector<Vector3> m_quadVs;
 
 	Camera m_camera;
 	Vector3 m_lightDirection;
@@ -60,16 +48,6 @@ private:
 	std::vector<Vector3> m_texelCenters;
 	bool m_isFirstFrame;
 
-	Vector3 c_indigo;
-	Vector3 c_white;
-	Vector3 c_grey;
-
-	std::vector<float> m_sphereRadii;
-
-	// Positional data of spheres
-	std::vector<float> m_sphereCentersX;
-	std::vector<float> m_sphereCentersY;
-	std::vector<float> m_sphereCentersZ;
 
 	std::vector<Triangle> m_faces;
 	Vector3 m_center;
